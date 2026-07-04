d130bed992353a1ed3176fb8fcc4ddb96857917a
Kasauti International <kasautiinternational@gmail.com>
2026-07-04 10:42:36 +0530

    Update settings.py
    DBUG:- TURE TO FALSE

--- kasauti_project/kasauti_project/settings.py
+++ kasauti_project/kasauti_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-kasauti-change-this-in-production-use-env-var'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
